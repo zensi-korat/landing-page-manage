fe7e29da672a5b2dddd3b93f499ae4baa12ce685
zensi-korat <zensi@simformsolutions.com>
2024-03-12 19:07:10 +0530

    completed footer

--- landing-page-manage/main.js
+++ landing-page-manage/main.js
@@ -5,14 +5,27 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+// const navToggle = document.querySelector(".mobile-nav-toggle");
+// const primaryNav = document.querySelector(".primary-navigation");
+// const primaryHeader = document.querySelector(".primary-header");
+
+// navToggle.addEventListener("click", () => {
+//   primaryNav.hasAttribute("data-visible")
+//     ? navToggle.setAttribute("aria-expanded", false)
+//     : navToggle.setAttribute("aria-expanded", true);
+//   primaryNav.toggleAttribute("data-visible");
+//   primaryHeader.toggleAttribute("data-overlay");
+// });
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expanded", false)
-    : navToggle.setAttribute("aria-expanded", true);
+  const isMenuVisible = primaryNav.hasAttribute("data-visible");
+
+  navToggle.setAttribute("aria-expanded", isMenuVisible);
+  document.body.classList.toggle("hidden", !isMenuVisible);
+
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
